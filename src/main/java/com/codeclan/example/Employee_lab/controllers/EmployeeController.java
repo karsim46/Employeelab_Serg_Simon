4f2c629e5a4fccb7d2915000267b16cda92a8af9
Simon <simonwatson176@gmail.com>
2021-09-07 11:12:46 +0100

    Controller complete

--- src/main/java/com/codeclan/example/Employee_lab/controllers/EmployeeController.java
+++ src/main/java/com/codeclan/example/Employee_lab/controllers/EmployeeController.java
@@ -4,7 +4,11 @@ import com.codeclan.example.Employee_lab.Model.Employee;
 import com.codeclan.example.Employee_lab.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
@@ -12,7 +16,13 @@ public class EmployeeController {
     EmployeeRepository employeeRepository;
 
     @GetMapping(value = "/employees")
-    public List <Employee> getAllEmployees(){
-        
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping(value = "/employees/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable Long Id){
+        return employeeRepository.findById(Id);
+
     }
 }
